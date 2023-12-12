ef37b081a66dac4024ec960db870c2224d5288dc
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2023-12-12 14:06:52 +0100

    project set up

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+os.environ["CLOUDINARY_URL"] = "cloudinary://886743557967616:TdIMw6pKdMqdRjsNoiQTJI_6cl4@dj8ii3vdc"
+os.environ["SECRET_KEY"] = "v463*or^jelj3i00kok#006msh*$9oc1%$(qnq6ey()o16+fo-"
