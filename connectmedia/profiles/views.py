8cb74eee5bc37ad5161f8fca84570863c1f4d5a2
LabyKreative <labykreative@yahoo.com>
2023-12-19 22:07:40 +0100

    Refactor profile views to use generics and add filtering and ordering options

--- connectmedia/profiles/views.py
+++ connectmedia/profiles/views.py
@@ -1,48 +1,46 @@
-"""ConnectMedia Views"""
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+"""Profile views."""
+from django.db.models import Count
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters, generics
 from connectmedia.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
 
 
 # Create your views here.
-class ProfileList(APIView):
-    """List all profiles or create a new profile."""
-    def get(self, request):
-        """Get all profiles."""
-        profiles = Profile.objects.all()
-        serializer = ProfileSerializer(profiles, many=True, context={'request': request})
-        return Response(serializer.data)
+class ProfileList(generics.ListAPIView):
+    """Profile list view."""
+
+    queryset = Profile.objects.annotate(
+        posts_count=Count("owner__post", distinct=True),
+        followers_count=Count("owner__followed", distinct=True),
+        following_count=Count("owner__following", distinct=True),
+    ).order_by("-created_at")
+    serializer_class = ProfileSerializer
+    filter_backends = [
+        filters.OrderingFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = [
+        "owner__following__followed__profile",
+        "owner__followed__owner__profile",
+    ]
+    ordering_fields = [
+        "posts_count",
+        "followers_count",
+        "follwoing_count",
+        "owner__following__created_at",
+        "owner__followed__created_at",
+    ]
 
 
-class ProfileDetail(APIView):
-    """Retrieve, update or delete a profile instance."""
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """Profile detail view."""
+
+    queryset = Profile.objects.annotate(
+        posts_count=Count("owner__post", distinct=True),
+        followers_count=Count("owner__followed", distinct=True),
+        following_count=Count("owner__following", distinct=True),
+    ).order_by("-created_at")
     serializer_class = ProfileSerializer
     permission_classes = [IsOwnerOrReadOnly]
-
-    def get_object(self, pk):
-        """Get a profile object."""
-        try:
-            profile = Profile.objects.get(pk=pk)
-            self.check_object_permissions(self.request, profile)
-            return profile
-        except Profile.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk):
-        """Get a profile."""
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, context={'request': request})
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        """Update a profile."""
-        profile = self.get_object(pk)
-        serializer = ProfileSerializer(profile, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
