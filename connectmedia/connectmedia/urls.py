682d707df7d2bb9a3f3b23ced0e53f59ea566531
Blessing Chukwunwike <117754837+BEChukwunwike@users.noreply.github.com>
2023-12-20 17:12:13 +0100

    Refactor code to improve performance and readability

--- connectmedia/connectmedia/urls.py
+++ connectmedia/connectmedia/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from .views import home_view
 
 urlpatterns = [
+    path("", home_view, name='home'),
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("", include("profiles.urls")),
