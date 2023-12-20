ec314cb2931a962991ba7233aa882bc49bb55396
LabyKreative <labykreative@yahoo.com>
2023-12-20 04:10:14 +0100

    Add like functionality to PostSerializer and following functionality to ProfileSerializer

--- connectmedia/posts/serializers.py
+++ connectmedia/posts/serializers.py
@@ -1,45 +1,59 @@
 """Posts serializers."""
 from rest_framework import serializers
-from posts.models import Post
+from likes.models import Like
+from .models import Post
 
 
+# Create your serializers here.
 class PostSerializer(serializers.ModelSerializer):
-    """Post model serializer."""
+    """Post serializer."""
+
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source="owner.profile.id")
     profile_picture = serializers.ReadOnlyField(source="owner.profile.picture.url")
+    like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
     def validate_picture(self, value):
-        """Validate picture"""
+        """Validate picture."""
         if value.size > 2 * 1024 * 1024:
-            raise serializers.ValidateError(
-                "Image file is too large ( > 2mb)"
-            )
+            raise serializers.ValidationError("Image file too large ( > 2mb )")
         if value.image.width > 4096 or value.image.height > 4096:
-            raise serializers.ValidateError(
-                "Image height or width is too large"
-            )
+            raise serializers.ValidationError("Image height or width is too large")
         return value
 
     def get_is_owner(self, obj):
-        """Check if the user is the owner."""
-        request = self.context.get('request')
+        """Get is owner."""
+        request = self.context["request"]
         return request.user == obj.owner
+
+    def get_like_id(self, obj):
+        """Get like id."""
+        user = self.context["request"].user
+        if user.is_authenticated:
+            like = Like.objects.filter(owner=user, post=obj).first()
+            return like.id if like else None
+        return None
 
     class Meta:
         """Meta class."""
+
         model = Post
         fields = [
-            'id',
-            'owner',
-            'is_owner',
-            'profile_id',
-            'profile_picture',
-            'created_at',
-            'modified_at',
-            'title',
-            'content',
-            'picture',
-            'image_filter',
+            "id",
+            "owner",
+            "is_owner",
+            "profile_id",
+            "profile_picture",
+            "created_at",
+            "modified_at",
+            "title",
+            "content",
+            "picture",
+            "image_filter",
+            "like_id",
+            "likes_count",
+            "comments_count",
         ]
--- connectmedia/profiles/serializers.py
+++ connectmedia/profiles/serializers.py
@@ -1,28 +1,48 @@
-"""Profiles serializers."""
+"""Profile serializers."""
 from rest_framework import serializers
+from followers.models import Follower
 from .models import Profile
 
 
+# Create your serializers here.
 class ProfileSerializer(serializers.ModelSerializer):
-    """Profile model serializer."""
-    owner = serializers.ReadOnlyField(source='owner.username')
+    """Profile serializer."""
+
+    owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
+    posts_count = serializers.ReadOnlyField()
+    followers_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
-        """Check if the user is the owner."""
-        request = self.context.get('request')
-        return obj.owner == request.user
+        """Get is owner."""
+        request = self.context["request"]
+        return request.user == obj.owner
+
+    def get_following_id(self, obj):
+        """Get following id."""
+        user = self.context["request"].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(owner=user, followed=obj.owner).first()
+            return following.id if following else None
+        return None
 
     class Meta:
         """Meta class."""
+
         model = Profile
         fields = [
-            'id',
-            'owner',
-            'created_at',
-            'modified_at',
-            'name',
-            'content',
-            'picture',
-            'is_owner',
+            "id",
+            "owner",
+            "created_at",
+            "modified_at",
+            "name",
+            "content",
+            "picture",
+            "is_owner",
+            "following_id",
+            "posts_count",
+            "followers_count",
+            "following_count",
         ]
