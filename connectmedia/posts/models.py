5a6c075095caa3afa4e8ecb50ccd642b7f09b7aa
LabyKreative <labykreative@yahoo.com>
2023-12-25 15:17:34 +0100

    Refactor posts

--- connectmedia/posts/models.py
+++ connectmedia/posts/models.py
@@ -6,31 +6,32 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     """Post model."""
+
     image_filter_choices = [
-        ('_2023', '2023'),
-        ('jpsconnect', 'Japan'),
-        ('indconnect', 'India'),
-        ('grcconnect', 'Greece'),
-        ('thnconnect', 'Thailand'),
-        ('turkconnect', 'Turkey'),
-        ('usaconnect', 'USA'),
-        ('mexconnect', 'Mexico'),
-        ('itaconnect', 'Italy'),
-        ('9jaconnect', 'Nigeria'),
-        ('chnconnect', 'China'),
-        ('franceconnect', 'France'),
-        ('spnconnect', 'Spanish'),
-        ('brconnect', 'Brazil'),
-        ('rusconnect', 'Russia'),
-        ('canconnect', 'Canada'),
-        ('ausconnect', 'Australia'),
-        ('indonconnect', 'Indonesia'),
-        ('gerconnect', 'Germany'),
-        ('ukconnect', 'UK'),
-        ('kconnect', 'Korea'),
-        ('saconnect', 'South Africa'),
-        ('uaeconnect', 'UAE'),
-        ('egconnect', 'Egypt'),
+        ("_2023", "2023"),
+        ("jpsconnect", "Japan"),
+        ("indconnect", "India"),
+        ("grcconnect", "Greece"),
+        ("thnconnect", "Thailand"),
+        ("turkconnect", "Turkey"),
+        ("usaconnect", "USA"),
+        ("mexconnect", "Mexico"),
+        ("itaconnect", "Italy"),
+        ("9jaconnect", "Nigeria"),
+        ("chnconnect", "China"),
+        ("franceconnect", "France"),
+        ("spnconnect", "Spanish"),
+        ("brconnect", "Brazil"),
+        ("rusconnect", "Russia"),
+        ("canconnect", "Canada"),
+        ("ausconnect", "Australia"),
+        ("indonconnect", "Indonesia"),
+        ("gerconnect", "Germany"),
+        ("ukconnect", "UK"),
+        ("kconnect", "Korea"),
+        ("saconnect", "South Africa"),
+        ("uaeconnect", "UAE"),
+        ("egconnect", "Egypt"),
     ]
     # Sort the list alphabetically by the second item in the tuple
     image_filter_choices.sort(key=lambda x: x[1])
@@ -44,13 +45,14 @@ class Post(models.Model):
         upload_to="images/", default="../default_post_zuk6nv", blank=True
     )
     image_filter = models.CharField(
-        max_length=100, choices=image_filter_choices, default='normal'
+        max_length=100, choices=image_filter_choices, default="normal"
     )
 
     class Meta:
         """Meta class."""
+
         ordering = ["-created_at"]
 
     def __str__(self):
-        """Return name and username."""
+        """Return title and username."""
         return f"{self.id} {self.title}"
