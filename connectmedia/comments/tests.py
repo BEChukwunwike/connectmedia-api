9628ba9d6d671a9a0e25ce7f91eaaaf57b68a118
LabyKreative <labykreative@yahoo.com>
2023-12-25 14:45:48 +0100

    Test comments

--- connectmedia/comments/tests.py
+++ connectmedia/comments/tests.py
@@ -9,7 +9,9 @@ from .models import Comment
 
 # Create your tests here.
 class CommentListTestCase(TestCase):
+    """Tests for the CommentList view."""
     def setUp(self):
+        """Set up the test client and create a user."""
         self.client = APIClient()
         self.user = User.objects.create_user(username="testuser", password="testpass")
         self.client.login(username="testuser", password="testpass")
@@ -17,11 +19,13 @@ class CommentListTestCase(TestCase):
         self.url = reverse("comment-list")
 
     def test_list_comments(self):
+        """Test that a list of comments can be retrieved."""
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
 
     def test_create_comment(self):
+        """Test that a comment can be created."""
         data = {"content": "New comment"}
         response = self.client.post(self.url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -30,6 +34,7 @@ class CommentListTestCase(TestCase):
 
 
 class CommentDetailTestCase(TestCase):
+    """Tests for the CommentDetail view."""
     def setUp(self):
         self.client = APIClient()
         self.user = User.objects.create_user(username="testuser", password="testpass")
@@ -38,11 +43,13 @@ class CommentDetailTestCase(TestCase):
         self.url = reverse("comment-detail", kwargs={"pk": self.comment.id})
 
     def test_retrieve_comment(self):
+        """Test that a comment can be retrieved."""
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data["content"], "Test comment")
 
     def test_update_comment(self):
+        """Test that a comment can be updated."""
         data = {"content": "Updated comment"}
         response = self.client.put(self.url, data, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -51,6 +58,7 @@ class CommentDetailTestCase(TestCase):
         )
 
     def test_delete_comment(self):
+        """Test that a comment can be deleted."""
         response = self.client.delete(self.url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Comment.objects.count(), 0)
