c8f5fd8e7517804342cc2c65b09c024051f0b959
LabyKreative <labykreative@yahoo.com>
2024-01-08 13:53:12 +0100

    Add static files and update settings.py

--- connectmedia/settings.py
+++ connectmedia/settings.py
@@ -198,7 +198,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
